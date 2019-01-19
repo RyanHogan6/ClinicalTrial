bfc77d51fb33b4f3b56f8730053ab90bff9ef10c
Ryan Hogan <ryan.hogan16@stjohns.edu>
2019-01-19 00:43:01 -0500

    Initial commit

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -1,5 +1,25 @@
+/*
+    This is the javascript file that essentially runs all of our backend.
+    All of our API requests, as well as our routing is done through this file.
+ */
+
+//  Initializes our angular application, in which we include the below libraries for routing
+//  and other functions
 var app = angular.module('clinicalTrialApp', ['ui.router', 'ngMaterial', 'ngAnimate']);
 
+/**
+ * This is the setup for our ui routing component, it allows us to define our roots and states of our
+ * application. We pass the url and route providers in order to dynamically change url parameters, as well as
+ * $window in order to directly access the window of our page.
+ *
+ * Each 'state' below offers us a potential landing page that we can access depending on a trigger or an
+ * action in the application.
+ *
+ * In a state below, you have the state name, in which it will be referenced. You have the url, which is the actual
+ * url you will reference. The template url, which is the name of the file you want to pull the HTML code from. And lastly,
+ * you can call controller that you made in order to access a group of functions you defined, almost like inheriting a class.
+ * You can add functions right in the state if you want, or call them, both are done below.
+ */
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider, $window) {
 
     $stateProvider
@@ -57,49 +77,6 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
                     $window.history.back();
                 };
 
-                var geocoder;
-                var map;
-
-                $scope.initMap = function (city, state) {
-
-                    console.log(city + "city" + " ___ " + state + "state");
-
-                    var address = city + "," + state;
-
-                    geocoder = new google.maps.Geocoder();
-                    var latlng = new google.maps.LatLng(-34.397, 150.644);
-                    var myOptions = {
-                        zoom: 8,
-                        center: latlng,
-                        mapTypeControl: true,
-                        mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-                        navigationControl: true,
-                        mapTypeId: google.maps.MapTypeId.ROADMAP
-                    };
-
-                    map = new google.maps.Map(document.getElementById("map"), myOptions);
-
-                    if (geocoder) {
-                        geocoder.geocode( { 'address': address}, function(results, status) {
-                            if (status === google.maps.GeocoderStatus.OK) {
-                                if (status !== google.maps.GeocoderStatus.ZERO_RESULTS) {
-                                    map.setCenter(results[0].geometry.location);
-
-                                    var marker = new google.maps.Marker({
-                                        position: results[0].geometry.location,
-                                        map: map,
-                                        title: address
-                                    });
-                                    google.maps.event.addListener(marker, 'click', function () {
-                                        infowindow.open(map, marker);
-                                    });
-
-                                }
-                            }
-                        });
-                    }
-                };
-
                 $scope.hideConflicts = function (nct) {
                     $scope.empty = "";
                     $http.get("http://localhost:8080/clin/clinical/getConflicts/" + nct).then(function (response) {
@@ -129,9 +106,13 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
         })
 
         .state('viewTrials.map', {
-            templateUrl: 'map.html'
+            templateUrl: 'scratchFiles/map.html'
         })
 
+        /**
+         * As you can see below, we call a view, which is basically a sub section of a state. So if we are
+         * in the fullEligCrit state, we want to call another view into the state without leaving the view.
+         */
         .state('viewTrials.fullEligCrit', {
             views: {
                 'fullCrit': {
@@ -160,19 +141,22 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
             }
         });
 
-
-
+    // This is the default page to land on if all else fails
     $urlRouterProvider.otherwise('/searchBar');
 }]);
 
 
+/**
+ * Controllers allow you to section off a particular job into one area so you can reference these
+ * functions exclusively to a state or view. Almost like having a specific class for a job.
+ */
 app.controller('autoCompleteController', function($scope, $http) {
-
 
     $scope.conditionList = [];
     var data = "";
     var temp = [];
 
+    // Gets all conditions and then formats them to be used in our autocomplete
     $http.get('http://localhost:8080/clin/clinical/getAllConditions').then(function(response) {
           data = response.data.toString();
           temp = data.toString().replace("[", "").replace("]", "").split(",");
@@ -180,7 +164,10 @@ app.controller('autoCompleteController', function($scope, $http) {
     });
 
 
-
+    /*
+        Filters the value that is being typed into the search bar in order to
+        keep checking the autocomplete search.
+     */
     $scope.cname = function(string) {
 
         $scope.hidethis = false;
@@ -211,6 +198,10 @@ app.controller('autoCompleteController', function($scope, $http) {
 
     };
 
+    /*
+        Fills the search bar with the value when found. Also triggers the dropdown box to
+        hide when the value is found.
+     */
     $scope.fillInputBox = function(string) {
 
         $scope.condition = string;
@@ -218,8 +209,6 @@ app.controller('autoCompleteController', function($scope, $http) {
         $scope.hidethis = true;
         $scope.hideBox = false;
         $scope.disable = false;
-
-
     };
 
     $scope.hideConflicts = function(nct) {
